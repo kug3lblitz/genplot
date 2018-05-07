7fb42bccde86a6288a3a4242f5b0065be0dcd1b2
Patrick Werner <kug3lblitz@protonmail.com>
2018-05-07 18:55:38 -0400

    randomwalks now generate new versions when prompted

--- randomwalks/rw_visual.py
+++ randomwalks/rw_visual.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-# make a random walk and plot the points
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# keep creating random walks while active
+while True:
+    # make a random walk and plot the points
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Generate another walk? (y/n):")
+    if keep_running == 'n':
+        break

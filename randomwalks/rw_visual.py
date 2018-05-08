d4d179d610b6624abd458d7db4bae1fec0a05188
Patrick Werner <kug3lblitz@protonmail.com>
2018-05-07 21:38:36 -0400

    made chart clearer, resolution options

--- randomwalks/rw_visual.py
+++ randomwalks/rw_visual.py
@@ -4,9 +4,26 @@ from random_walk import RandomWalk
 # keep creating random walks while active
 while True:
     # make a random walk and plot the points
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    # set size of plotting window (optional)
+    # plt.figure(figsize=(10, 6))
+    plt.figure(dpi=128, figsize=(10, 6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+        edgecolor='none', s=5)
+
+    # emphasize the first and last points
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red',
+        edgecolors='none', s=100)
+
+    # remove the axes (optional)
+    # plt.axes().get_xaxis().set_visible(False)
+    # plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Generate another walk? (y/n):")
